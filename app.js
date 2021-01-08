65ca22c22cdaf5abe1011c115520630d6aaa48e0
natanaelrusli <64279959+natanaelrusli@users.noreply.github.com>
2021-01-08 10:48:19 +0700

    Simplify

--- app.js
+++ app.js
@@ -5,13 +5,12 @@ const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser');
 const session = require('express-session')
 
-// DB Config
+// MongoDB Connection String
 const mongoURI = "mongodb+srv://admin:admin@cluster0.5htwy.mongodb.net/user?retryWrites=true&w=majority"
 
-// Connect to mongoDB
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true})
+// Connect to MongoDB using connection string and mongoose
+mongoose.connect(mongoURI)
     .then(() => console.log('MongoDB Connected'))
-    .catch((err) => console.log(err))
 
 // EJS
 // Make sure calling the express layouts before setting the view engine
@@ -21,13 +20,22 @@ app.set('view engine', 'ejs')
 // Bodyparser
 app.use(express.urlencoded({ extended: false }))
 
+// This is required to login and keep user data across pages
 app.use(cookieParser());
 app.use(session({secret: "secret"}));
 
 // Routes
-app.use('/', require('./routes/index'))
 app.use('/users', require('./routes/users'))
 
+app.get('/', (req,res) => {
+
+    if (typeof req.session.email != 'undefined') {
+        res.render('index', { user : req.session })
+    } else {
+        res.render('login')
+    }
+    
+})
 
 const port = 5000;
 app.listen(port, console.log(`Server started on port ${port}`))
